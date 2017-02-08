15f0751eb467c0cf1db69de8bcb0c7689e077817
PLane002 <ptlane002@gmail.com>
2017-02-07 20:43:32 -0600

    Add files via upload

---
+++ project.sql
@@ -0,0 +1,23 @@
+/*
+	
+
+
+*/
+
+
+
+
+CREATE TABLE Account(
+	user_id INT NOT NULL,
+	role varchar(45) NOT NULL,
+	PRIMARY KEY (user_id)
+ );
+
+CREATE TABLE Gives(
+	date_published DATE NOT NULL
+	
+);
+
+
+
+
